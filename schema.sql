63a9f6bd27c0c290b0940cce5f1427528f914c92
Wendy <wendytsai1988@gmail.com>
2020-05-10 16:18:15 +0200

    debugging

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE list (
+CREATE TABLE lists (
   id serial PRIMARY KEY,
   list_name varchar(100) UNIQUE NOT NULL CHECK(length(list_name) > 0)
 );
 
-CREATE TABLE  todo (
+CREATE TABLE  todos (
   id serial PRIMARY KEY,
   todo_name varchar(100) NOT NULL CHECK(length(todo_name) > 0),
   completed boolean NOT NULL DEFAULT false,
-  list_id integer NOT NULL REFERENCES list(id) 
+  list_id integer NOT NULL REFERENCES lists(id) 
 );
